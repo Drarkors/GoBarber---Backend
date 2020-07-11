a2b47928ce95865624419917498694d8d770ef85
Drarkors <rafaelfzanetti@hotmail.com>
2020-07-11 02:23:14 -0300

    "Fist commit"

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '4d8ecd6f89bdaa73a880f4fb89066996',
+        expiresIn: '1d',
+    },
+};
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,56 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
+
+import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticad from '../middlewares/ensureAuthenticated';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', async (request, response) => {
+    try {
+        const { name, email, password } = request.body;
+
+        const createUser = new CreateUserService();
+
+        const user = await createUser.execute({
+            name,
+            email,
+            password,
+        });
+
+        delete user.password;
+
+        return response.json(user);
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+});
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticad,
+    upload.single('avatar'),
+    async (request, response) => {
+        try {
+            const updateUserAvatarService = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatarService.execute({
+                user_id: request.user.id,
+                avatarFilename: request.file.filename,
+            });
+
+            delete user.password;
+
+            return response.json(user);
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
+    },
+);
+
+export default usersRouter;
